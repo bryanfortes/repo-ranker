f8b3360e4b96fe9c5c38d2abc24458de088701df
bryanfortes <bryanfortes1@gmail.com>
2023-07-26 10:21:39 -0400

    Implemented Battle component

--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import "./index.css"
 import Popular from './components/popular'
+import Battle from './components/Battle'
 class App extends React.Component{
     render () {
         return <div className='light'>
             <div className='container'>
-                <Popular />
+                <Battle />
             </div>
         </div>
     }
