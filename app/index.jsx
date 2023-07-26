c0d75355f90c79d7d2ad25883261d71a339e9439
bryanfortes <bryanfortes1@gmail.com>
2023-07-26 10:21:39 -0400

    Implemented a Tooltip hover effect on returned repos from api call

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ class App extends React.Component{
     render () {
         return <div className='light'>
             <div className='container'>
-                <Battle />
+                <Popular />
             </div>
         </div>
     }
